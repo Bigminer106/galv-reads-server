9ebfdd2ead656c37bfcd4e4e042abcf13b4941ad
Adam Miner <miner.adam1@hotmail.com>
2017-07-12 10:47:51 -0600

    Heroku Work

--- knexfile.js
+++ knexfile.js
@@ -4,10 +4,6 @@ module.exports = {
 
   development: {
     client: 'pg',
-    connection: 'postgresql://localhost/galvReads'
-  },
-
-  staging: {
-    client: 'postgresql'
-  },
+    connection: 'postgres://butwgzvbbswzim:e080a097c42d297dd6bd4e43ee0a578f58757c1b309a99273dfc4b034f33a6bd@ec2-23-23-244-83.compute-1.amazonaws.com:5432/d372rq9ot6faq'
+  }
 };
