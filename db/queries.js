6f6bddf38e16a0e2a67bd5f1f7c2d0a5c4d9ba17
Adam Miner <miner.adam1@hotmail.com>
2017-07-12 11:43:45 -0600

    fixes

--- db/queries.js
+++ db/queries.js
@@ -1,54 +1,34 @@
 const knex = require('./knex');
 
 module.exports = {
-  {
-    getAll() {
-      return knex('books')
-    }
+  getAll() {
+    return knex('books')
   },
-  {
-    getAll() {
-      return knex('authors');
-    }
+  getAll() {
+    return knex('authors');
   },
-  {
-    getOne(id) {
-      return knex('books').where('id', id).first();
-    }
+  getOne(id) {
+    return knex('books').where('id', id).first();
   },
-  {
-    getOne(id) {
-      return knex('authors').where('id', id).first();
-    }
+  getOne(id) {
+    return knex('authors').where('id', id).first();
   },
-  {
-    create(book) {
-      return knex('books').insert(book, '*');
-    }
+  create(book) {
+    return knex('books').insert(book, '*');
   },
-  {
-    create(author) {
-      return knex('authors').insert(author, '*');
-    }
+  create(author) {
+    return knex('authors').insert(author, '*');
   },
-  {
-    update(id, book) {
-      return knex('books').where('id', id).update(book);
-    }
+  update(id, book) {
+    return knex('books').where('id', id).update(book);
   },
-  {
-    update(id, author) {
-      return knex('authors').where('id', id).update(author);
-    }
+  update(id, author) {
+    return knex('authors').where('id', id).update(author);
   },
-  {
-    delete(id) {
-      return knex('books').where('id', id).del();
-    }
+  delete(id) {
+    return knex('books').where('id', id).del();
   },
-  {
-    delete(id) {
-      return knex('authors').where('id', id).del();
-    }
+  delete(id) {
+    return knex('authors').where('id', id).del();
   }
 };
