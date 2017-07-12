7deefd204f4c276f9feb1ebb52f39a1aecbcf3bf
Adam Miner <miner.adam1@hotmail.com>
2017-07-12 11:12:00 -0600

    Tweaks

--- knexfile.js
+++ knexfile.js
@@ -9,5 +9,5 @@ module.exports = {
   production: {
     client: 'pg',
     connection: 'postgres://butwgzvbbswzim:e080a097c42d297dd6bd4e43ee0a578f58757c1b309a99273dfc4b034f33a6bd@ec2-23-23-244-83.compute-1.amazonaws.com:5432/d372rq9ot6faq'
-  };
+  }
 };
