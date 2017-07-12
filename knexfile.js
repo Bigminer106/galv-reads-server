7a0507b1bbc1aacc5b71f5fd9b3f0d230fd6bf30
Adam Miner <miner.adam1@hotmail.com>
2017-07-12 11:13:35 -0600

    Fixes

--- knexfile.js
+++ knexfile.js
@@ -4,10 +4,10 @@ module.exports = {
 
   development: {
     client: 'pg',
-    connection: 'postgres://butwgzvbbswzim:e080a097c42d297dd6bd4e43ee0a578f58757c1b309a99273dfc4b034f33a6bd@ec2-23-23-244-83.compute-1.amazonaws.com:5432/d372rq9ot6faq'
+    connection: 'postgres://butwgzvbbswzim:e080a097c42d297dd6bd4e43ee0a578f58757c1b309a99273dfc4b034f33a6bd@ec2-23-23-244-83.compute-1.amazonaws.com:5432/d372rq9ot6faqh'
   },
   production: {
     client: 'pg',
-    connection: 'postgres://butwgzvbbswzim:e080a097c42d297dd6bd4e43ee0a578f58757c1b309a99273dfc4b034f33a6bd@ec2-23-23-244-83.compute-1.amazonaws.com:5432/d372rq9ot6faq'
+    connection: 'postgres://butwgzvbbswzim:e080a097c42d297dd6bd4e43ee0a578f58757c1b309a99273dfc4b034f33a6bd@ec2-23-23-244-83.compute-1.amazonaws.com:5432/d372rq9ot6faqh'
   }
 };
