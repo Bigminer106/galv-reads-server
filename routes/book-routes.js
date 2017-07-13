c640b11ef59209ba2fe6561607b0aac35e0233b4
Adam Miner <miner.adam1@hotmail.com>
2017-07-12 18:43:42 -0600

    Rollback to last functioning

--- routes/book-routes.js
+++ routes/book-routes.js
@@ -2,21 +2,21 @@ const express = require('express');
 const router = express.Router();
 const queries = require('../db/books-queries.js');
 
-// function isValidID(req, res, next) {
-//   if(!isNaN(req.params.id)) {
-//     return next();
-//   } else {
-//     next(new Error('Invalid ID'));
-//   }
-// };
+function isValidID(req, res, next) {
+  if(!isNaN(req.params.id)) {
+    return next();
+  } else {
+    next(new Error('Invalid ID'));
+  }
+};
 
-// function validBook(book) {
-//   const hasTitle = typeof book.title == 'string' && book.title.trim() != '';
-//   const hasCover = typeof book.cover == 'string' && book.cover.trim() != '';
-//   const hasDescription = typeof book.description == 'string' && book.description.trim() != '';
-//   const hasGenre = typeof book.genre == 'string' && book.genre.trim() != '';
-//   return hasTitle && hasCover && hasDescription && hasGenre;
-// };
+function validBook(book) {
+  const hasTitle = typeof book.title == 'string' && book.title.trim() != '';
+  const hasCover = typeof book.cover == 'string' && book.cover.trim() != '';
+  const hasDescription = typeof book.description == 'string' && book.description.trim() != '';
+  const hasGenre = typeof book.genre == 'string' && book.genre.trim() != '';
+  return hasTitle && hasCover && hasDescription && hasGenre;
+};
 
 router.get('/', (req, res) => {
   queries.getAll().then(books => {
@@ -31,22 +31,22 @@ router.get('/', (req, res) => {
 // });
 
 router.post('/', (req, res, next) => {
-  // if (validBook(req.body)) {
+  if (validBook(req.body)) {
     queries.create(req.body).then(books => {
       res.json(books[0]);
-    // })
-  // } else {
-  //   next(new Error('Invalid Book'))
+    })
+  } else {
+    next(new Error('Invalid Book'))
   });
 })
 
 router.put('/:id', isValidID, (req, res, next) => {
-  // if (validBook(req.body)) {
+  if (validBook(req.body)) {
     queries.update(req.params.id, req.body).then(books => {
       res.json(books[0]);
-    // });
-  // } else {
-  //   next(new Error('Invalid Book'))
+    });
+  } else {
+    next(new Error('Invalid Book'))
   });
 });
 
