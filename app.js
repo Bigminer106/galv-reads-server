6e3ee32952b7e44ccfdfda4b20e33f3b95b87142
Adam Miner <miner.adam1@hotmail.com>
2017-07-13 08:47:10 -0600

    Fixes

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const books = require('./routes/book-routes.js');
 const authors = require('./routes/author-routes.js');
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
+app.use(cors());
 app.use('/api/v1/books', books);
 app.use('/api/v1/authors', authors);
 app.use(function(err, req, res, next) {
